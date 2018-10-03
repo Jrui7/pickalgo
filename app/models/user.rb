6047671d96eb69acebf6fe8dbea43f8f62f3df98
jorge <jorge.ruivinho@me.com>
2018-10-03 13:17:27 +0200

    add columns first_name and last_name to users, create devise views and change the form with the fileds first_name last name and acceptance of terms of use

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :terms_of_service, acceptance: { message: "Merci de valider les conditions d'utilisation" }
+  validates :first_name, presence: { message: "Merci de renseigner votre prénom" }
+  validates :last_name, presence: { message: "Merci de renseigner votre prénom" }
 end
