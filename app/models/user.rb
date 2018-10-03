0cb8108ddcb7a6181369d263cce3e0e6b533b1cd
jorge <jorge.ruivinho@me.com>
2018-10-03 22:47:08 +0200

    create welcome email and callbacks in user model to enrich mailing list and send welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,22 @@ class User < ApplicationRecord
   validates :last_name, presence: { message: "Merci de renseigner votre nom" }
   validates :email, presence: { message: "Merci de renseigner votre email" }
   validates :email, uniqueness: true
+
+  after_create :send_welcome_email
+  after_create :subscribe_to_newsletter
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+    # UserMailer.welcome(self.id).deliver_later(wait_until: 1.day.from_now)
+  end
+
+  def subscribe_to_newsletter
+    AddUserToMarketingListJob.perform_later(self.id)
+  end
+
+
+
+
 end
