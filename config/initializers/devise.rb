78c5e1b77ed6cddfd3e519dd32049e81e1c22af6
jorge <jorge.ruivinho@me.com>
2018-10-04 22:39:55 +0200

    change email default sender for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '8271d34e8c05dab4842fc7c2b437f4488051453c0127ac87543274ddaa334c3b02918853eb14046fbb028271956c3b3f722c2f55ebde928798f13df6c265be1d'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+ config.mailer_sender = '"Pickalgo" <contact@pickalgo.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
