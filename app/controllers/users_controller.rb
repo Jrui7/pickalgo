42aac1081e1c767073a96cc8bd1877b8607851ae
jorge <jorge.ruivinho@me.com>
2018-10-04 20:42:39 +0200

    profil created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.friendly.find(params[:id])
   end
+
+  def update
+    @user = User.friendly.find(params[:id])
+    @user.slug = nil
+    if @user.update(user_params)
+      respond_to do |format|
+        format.html {redirect_to user_path(@user)}
+        format.js
+      end
+      @user.friendly_id
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
 end
