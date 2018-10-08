04a84be3eb1aecc6e8f8f4d83cbac4703509c798
jorge <jorge.ruivinho@me.com>
2018-10-08 11:19:43 +0200

    protect all application

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   before_action :set_page_action, only: [:faq, :contact, :mentions_legales, :cgu]
-  before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:home, :faq, :contact, :mentions_legales, :cgu]
 
   def home
