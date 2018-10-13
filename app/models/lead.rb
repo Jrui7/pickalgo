c87f7c7c09c529b55cb7177e6818d740384a3cce
jorge <jorge.ruivinho@me.com>
2018-10-13 17:25:48 +0200

    job enriching trello board with lead infos works in local

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -4,4 +4,12 @@ class Lead < ApplicationRecord
   validates :brand, presence: { message: "Merci de renseigner le nom de votre entreprise, marque, service..." }
   validates :contact_person, presence: { message: "Merci de renseigner le nom de la personne à contacter" }
 
+  after_create :send_lead_to_trello
+
+  private
+
+  def send_lead_to_trello
+    TrelloJob.perform_later(self.id)
+  end
+
 end
