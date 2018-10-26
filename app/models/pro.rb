8e054a53b90dfe81ab48605957a3dcfcfcf52dea
jorge <jorge.ruivinho@me.com>
2018-10-26 18:34:26 +0200

    save dashboard pro

--- app/models/pro.rb
+++ app/models/pro.rb
@@ -3,6 +3,10 @@ class Pro < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
+  validates :email, presence: { message: "Merci de renseigner l'email du contact" }
+  validates :email, uniqueness: true
+  validates :brand, presence: { message: "Merci de renseigner le nom de votre entreprise, marque, service..." }
+  validates :contact_person, presence: { message: "Merci de renseigner le nom de la personne à contacter" }
 
  extend FriendlyId
   friendly_id :brand, use: :slugged
