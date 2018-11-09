b754c3b3235d55eb5e4537e16eefe37830ec43dc
jorge <jorge.ruivinho@me.com>
2018-11-09 13:44:05 +0100

    save product show style, now able to see if i saw the product and if i picked it

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -6,4 +6,12 @@ class Pick < ApplicationRecord
   def self.pending_picks(user)
     where(user_id: user, state: "pending", card: {}).select {|pick| pick.campaign.ongoing?}
   end
+
+  def self.pending?(user, campaign)
+    where(user_id: user, campaign_id: campaign, state: "pending").any?
+  end
+
+  def self.view?(user, campaign)
+    where(user_id: user, campaign_id: campaign).any?
+  end
 end
