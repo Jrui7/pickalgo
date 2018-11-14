2a25cc7596946204d8c2b4eedc51adc08f779de0
jorge <jorge.ruivinho@me.com>
2018-11-14 18:37:16 +0100

    able to save new card, now need to process payment

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,34 @@
 class AddressesController < ApplicationController
   def create
     url = Rails.application.routes.recognize_path(request.referrer)
-    pick = Pick.find(url[:pick_id])
-    @address = Address.new(address_params_form_payment)
-    @address.user_id = current_user.id
-    authorize @address
-    if @address.save
-      flash[:notice] = "Adresse enregistrée"
-      respond_to do |format|
-        format.html { redirect_to new_pick_reservation_path(pick, anchor: 'reservation-box')}
+    if url[:controller] == "picks"
+      pick  = Pick.find(url[:id])
+      @address = Address.new(address_params_form_payment)
+      @address.user_id = current_user.id
+      authorize @address
+      if @address.save
+        flash[:notice] = "Adresse enregistrée"
+        respond_to do |format|
+          format.html { redirect_to edit_pick_path(pick, anchor: 'reservation-box')}
+        end
+      else
+        flash[:alert] = "Erreur: adresse non enregistrée"
+        redirect_to edit_pick_path(pick)
       end
     else
-      flash[:alert] = "Erreur: adresse non enregistrée"
-      redirect_to new_pick_reservation_path(pick)
+      pick = Pick.find(url[:pick_id])
+      @address = Address.new(address_params_form_payment)
+      @address.user_id = current_user.id
+      authorize @address
+      if @address.save
+        flash[:notice] = "Adresse enregistrée"
+        respond_to do |format|
+          format.html { redirect_to new_pick_reservation_path(pick, anchor: 'reservation-box')}
+        end
+      else
+        flash[:alert] = "Erreur: adresse non enregistrée"
+        redirect_to new_pick_reservation_path(pick)
+      end
     end
 
   end
