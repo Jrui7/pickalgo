f8c688b7513fa19baacb40c406c320b85e539d74
jorge <jorge.ruivinho@me.com>
2018-11-15 15:02:25 +0100

    paiement works and email works too

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -13,6 +13,13 @@ class CampaignMailer < ApplicationMailer
     mail to: @user.email, subject: "Commande confirmée", from: '"Pickalgo" <contact@pickalgo.com>'
   end
 
+    def success_later(user_id, campaign_id, pick_id)
+    @user = User.find(user_id)
+    @campaign = Campaign.find(campaign_id)
+    @pick = Pick.find(pick_id)
+    mail to: @user.email, subject: "Commande confirmée", from: '"Pickalgo" <contact@pickalgo.com>'
+  end
+
   def payment_error(user_id, campaign_id, pick_id)
     @user = User.find(user_id)
     @pick = Pick.find(pick_id)
