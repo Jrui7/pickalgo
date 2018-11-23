5feca59ca6099ee5141d1a7fb7f0e3ac7a8deb51
jorge <jorge.ruivinho@me.com>
2018-11-23 16:10:54 +0100

    save migration size boolean product, still need to style form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_093852) do
+ActiveRecord::Schema.define(version: 2018_11_23_144857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_093852) do
     t.string "youtube"
     t.string "twitter"
     t.string "size_guide"
+    t.boolean "size"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["pro_id"], name: "index_products_on_pro_id"
   end
