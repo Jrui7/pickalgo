9818cb14af20b91b4f53e1ae5b8aa1b58d341ad8
jorge <jorge.ruivinho@me.com>
2018-11-23 18:52:18 +0100

    form ok

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $(".form-choice").click(function(){
     $(".form-choice").toggleClass("active");
   });
+
+  $(".size-radio-input").click(function(){
+    $(".size-radio-input").removeClass("active");
+    $(this).toggleClass("active");
+  });
 });
